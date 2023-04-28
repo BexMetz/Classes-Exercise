3788fbd412dfbbfcae401038309cb915afe1c1fe
Bex <bexmetzger@gmail.com>
2023-04-28 10:59:54 -0700

    Exercise 1 Complete

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car bexCar = new Car();
+            bexCar.Make = "Honda";
+            bexCar.Model = "Civic";
+            bexCar.Year = 2017;
+
+            Car brettCar = new Car();
+            brettCar.Make = "Nisson";
+            brettCar.Model = "Optima";
+            brettCar.Year = 2015;
+
+            Car ianCar = new Car();
+            ianCar.Make = "Volkswagen";
+            ianCar.Model = "Jetta";
+            ianCar.Year = 2009;
+
+            var houseCars = new List<Car>() { bexCar, brettCar, ianCar };
+
+            foreach(var residentCar in houseCars)
+            {
+                Console.WriteLine($"{residentCar.Make} {residentCar.Model} {residentCar.Year}");
+            }
         }
     }
 }
